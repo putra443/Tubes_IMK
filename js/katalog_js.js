7e6b6bffffdf7dbc1cd718abb9a0eab2dc14947e
putra443 <putra443@gmail.com>
2021-12-03 18:07:22 +0700

    desain
    nambah desain

---
+++ js/katalog_js.js
@@ -0,0 +1,13 @@
+function directPageFilterTitle(){
+    window.location.replace("../Page/katalogSortedTitle.html");
+}
+function directPageFilterAuthor(){
+    window.location.replace("../Page/katalog.html");
+}
+function directPageFilterRating1(){
+    window.location.replace("../Page/katalog.html");
+}
+function directPageFilterRating2(){
+    window.location.replace("../Page/katalog.html");
+}
+
