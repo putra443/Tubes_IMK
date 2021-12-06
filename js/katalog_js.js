8697a4c712bcefa08fbff2d285fe3b8f48c3991e
putra443 <putra443@gmail.com>
2021-12-06 16:06:15 +0700

    dummy katalog
    nambah 4 buku

--- js/katalog_js.js
+++ js/katalog_js.js
@@ -11,3 +11,14 @@ function directPageFilterRating2(){
     window.location.replace("../Page/katalog.html");
 }
 
+let books = [
+    {title:"Book Cover Design Formula", tag:"BookCover", price:"120000", author:"Anita Nipane" , image_url:"../Image/61ZKNw0xixL.jpg", in_cart:"0"
+    },{
+        title:"Alices Adventures in Wonderland", tag:"Alice",  price:"111000", author:"Barnes & Noble" , image_url:"../Image/61ZKNw0xixL.jpg", in_cart:"0"
+    },{
+        title:"Dracula", tag:"Dracula", price:"200000", author:"Bram Stoker" , image_url:"../Image/61ZKNw0xixL.jpg", in_cart:"0"
+    },{
+        title:"Cats vs Dogs", tag:"cats", price:"150000", author:"Elizabeth Carney" , image_url:"../Image/61ZKNw0xixL.jpg", in_cart:"0"
+    }
+];
+
