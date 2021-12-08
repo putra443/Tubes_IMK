57fff946053d19482ec803d8032fafe35e485099
remalyaas <95465186+remalyaas@users.noreply.github.com>
2021-12-08 16:35:58 +0700

    up faq

--- js/FAQ.js
+++ js/FAQ.js
@@ -12,7 +12,32 @@ function slideumum() {
         dropdown.style.display = "flex";
     }
 }
+function slideumum2() {
+    var a = document.getElementById("ikonumum2");
+    var dropdown = document.getElementById("askumum2");
 
+    if (a.className == "fa fa-angle-double-up") {
+        a.className = "fa fa-angle-double-down";
+        dropdown.style.display = "none";
+    }
+    else {
+        a.className = "fa fa-angle-double-up";
+        dropdown.style.display = "flex";
+    }
+}
+function slideumum3() {
+    var a = document.getElementById("ikonumum3");
+    var dropdown = document.getElementById("askumum3");
+
+    if (a.className == "fa fa-angle-double-up") {
+        a.className = "fa fa-angle-double-down";
+        dropdown.style.display = "none";
+    }
+    else {
+        a.className = "fa fa-angle-double-up";
+        dropdown.style.display = "flex";
+    }
+}
 function slidepay() {
     var a = document.getElementById("ikonpay1");
     var dropdown = document.getElementById("askpay1");
@@ -30,6 +55,20 @@ function slidepay() {
 function slidepesan() {
     var a = document.getElementById("ikonpesan1");
     var dropdown = document.getElementById("askpesan1");
+
+    if (a.className == "fa fa-angle-double-up") {
+        a.className = "fa fa-angle-double-down";
+        dropdown.style.display = "none";
+    }
+    else {
+        a.className = "fa fa-angle-double-up";
+        dropdown.style.display = "flex";
+    }
+}
+
+function slidelain() {
+    var a = document.getElementById("ikonlain1");
+    var dropdown = document.getElementById("asklain1");
 
     if (a.className == "fa fa-angle-double-up") {
         a.className = "fa fa-angle-double-down";
