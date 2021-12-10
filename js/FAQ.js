3c716b3688ac040729701be850ae43e4738f3b35
remalyaas <95465186+remalyaas@users.noreply.github.com>
2021-12-10 15:17:10 +0700

    update FAQ

--- js/FAQ.js
+++ js/FAQ.js
@@ -38,7 +38,7 @@ function slideumum3() {
         dropdown.style.display = "flex";
     }
 }
-function slidepay() {
+function slidepay1() {
     var a = document.getElementById("ikonpay1");
     var dropdown = document.getElementById("askpay1");
 
@@ -51,8 +51,21 @@ function slidepay() {
         dropdown.style.display = "flex";
     }
 }
+function slidepay2() {
+    var a = document.getElementById("ikonpay2");
+    var dropdown = document.getElementById("askpay2");
 
-function slidepesan() {
+    if (a.className == "fa fa-angle-double-up") {
+        a.className = "fa fa-angle-double-down";
+        dropdown.style.display = "none";
+    }
+    else {
+        a.className = "fa fa-angle-double-up";
+        dropdown.style.display = "flex";
+    }
+}
+
+function slidepesan1() {
     var a = document.getElementById("ikonpesan1");
     var dropdown = document.getElementById("askpesan1");
 
@@ -65,10 +78,36 @@ function slidepesan() {
         dropdown.style.display = "flex";
     }
 }
+function slidepesan2() {
+    var a = document.getElementById("ikonpesan2");
+    var dropdown = document.getElementById("askpesan2");
 
-function slidelain() {
+    if (a.className == "fa fa-angle-double-up") {
+        a.className = "fa fa-angle-double-down";
+        dropdown.style.display = "none";
+    }
+    else {
+        a.className = "fa fa-angle-double-up";
+        dropdown.style.display = "flex";
+    }
+}
+
+function slidelain1() {
     var a = document.getElementById("ikonlain1");
     var dropdown = document.getElementById("asklain1");
+
+    if (a.className == "fa fa-angle-double-up") {
+        a.className = "fa fa-angle-double-down";
+        dropdown.style.display = "none";
+    }
+    else {
+        a.className = "fa fa-angle-double-up";
+        dropdown.style.display = "flex";
+    }
+}
+function slidelain2() {
+    var a = document.getElementById("ikonlain2");
+    var dropdown = document.getElementById("asklain2");
 
     if (a.className == "fa fa-angle-double-up") {
         a.className = "fa fa-angle-double-down";
