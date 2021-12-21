4a00f9172c47299ba5938861664d806d468378ae
remalyaas <95465186+remalyaas@users.noreply.github.com>
2021-12-21 20:00:52 +0700

    update profil

--- js/scriptProfil.js
+++ js/scriptProfil.js
@@ -1,2 +1,5 @@
 let current_user = JSON.parse(localStorage.getItem('current_user')) ;
 document.getElementById('profil').innerHTML = "Hi, " + current_user.username;
+document.getElementById('username').innerHTML = "Username : " + current_user.username;
+document.getElementById('pwprofil').innerHTML = "Password : " + current_user.password;
+document.getElementById('email').innerHTML = "Email : " + current_user.username + "@gmail.com";
